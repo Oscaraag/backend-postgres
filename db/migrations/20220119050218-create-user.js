0b881dbe8751bcb9f15fd392e3dc3a9b7af22423
Oscar <oscaraag1493@gmail.com>
2022-01-24 01:35:44 -0400

    [add] deploy

--- db/migrations/20220119050218-create-user.js
+++ db/migrations/20220119050218-create-user.js
@@ -1,10 +1,39 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('./../models/user.model');
+const { USER_TABLE } = require('./../models/user.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'onlooker',
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
   async down(queryInterface) {
