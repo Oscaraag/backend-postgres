4d0b51152ea9d618ad2c8e5921fd0d4978e82810
Oscar <oscaraag1493@gmail.com>
2022-01-24 01:57:36 -0400

    [add] deploy

--- db/migrations/20220123235527-add-order.js
+++ db/migrations/20220123235527-add-order.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
-
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize');
 module.exports = {
   async up(queryInterface, Sequelize) {
     /**
@@ -11,7 +12,31 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
 
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'costumer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
